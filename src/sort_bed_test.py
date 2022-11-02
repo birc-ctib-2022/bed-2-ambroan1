7e1ffa87319a7741910445fd8f42cd952ff510fb
ambroan1 <112683392+ambroan1@users.noreply.github.com>
2022-11-02 18:25:09 +0100

    Changes to sort_bed

--- src/sort_bed_test.py
+++ src/sort_bed_test.py
@@ -23,5 +23,23 @@
 #        ]
 #    assert sort_chr(bedlines) == expected
 
+
+from bed import BedLine
+from sort_bed import sort_chr
+
 def test_1984():
     assert 2 + 2 == 4
+
+def test_sort_chr_general_case():
+    """Test sort_chr works with a general case"""
+    bedlines = [
+        BedLine(chrom='chr1', chrom_start=600, chrom_end=700, name='foo'),
+        BedLine(chrom='chr1', chrom_start=600, chrom_end=650, name='baz')
+        ]
+    expected = [
+        BedLine(chrom='chr1', chrom_start=600, chrom_end=650, name='baz'),
+        BedLine(chrom='chr1', chrom_start=600, chrom_end=700, name='foo')
+        ]
+    assert sort_chr(bedlines) == expected
+
+test_sort_chr_general_case()
