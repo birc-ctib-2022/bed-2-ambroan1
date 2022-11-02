fa8c9d50542ee61fca3c874c7a00700cc9dd30e1
ambroan1 <112683392+ambroan1@users.noreply.github.com>
2022-11-02 22:22:18 +0100

    changes to query

--- src/query_bed.py
+++ src/query_bed.py
@@ -15,8 +15,8 @@ def extract_region(features: list[BedLine],
     s, e = 0, N
     while s < e :
         mid = (s + e) // 2
-        #if features[mid].chrom_start == start or s == e:
-        #    break
+        if features[mid].chrom_start == start or s == e:
+            break
         if features[mid].chrom_start < start:
             s = mid + 1
         else: e = mid
@@ -26,6 +26,9 @@ def extract_region(features: list[BedLine],
     #    if features[i].chrom_start >= end:
     #        break
     #   index_e +=1
+
+    if s >= N:
+        return []
     
     #result = features[index_s: index_e]
     results = []
